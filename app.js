3e65da6924d9df4258ea9591cd250e11af4a00cf
esknder <eskndermulugeta3@gmail.com>
2026-03-20 22:28:26 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// importing the module
+
+const math =require("./maths");
+
+//using the functions from the module
+
+console.log("addition",math.add(5,3));
+console.log("substraction",math.substract(5,3));
